1c1bcbdabdd6589e7f6ed5cb8a3f5747ee61d7ac
Nicolas Posa <nicoposa57@gmail.com>
2023-11-08 10:49:34 -0300

    new script

---
+++ server/schema.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE app_todo;
+
+USE app_todo;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255)
+);
+
+CREATE TABLE todos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255),
+  completed BOOLEAN DEFAULT false,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE shared_todos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  todo_id INT NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (todo_id) REFERENCES todos(id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
